05ecc7fe4068b61a16872c8202e5703c74d86f8d
Sandeep Katika <sandeep.katika@wipro.com>
2018-06-29 14:15:50 +0530

    minor update45

--- junit-calc-maven/src/test/java/com/hdp/calculator/CalculatorTest.java
+++ junit-calc-maven/src/test/java/com/hdp/calculator/CalculatorTest.java
@@ -37,7 +37,7 @@ public class CalculatorTest {
 	@Test
 	public void testDivision() {
 		try {
-			int result = calculator.division(126, 63);
+			int result = calculator.division(140, 70);
 			Assert.assertEquals(2, result);
 		} catch(Exception e) {
 			//e.printStackTrace(System.err);
